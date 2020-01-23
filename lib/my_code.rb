106b6dd86177cfd5c0612b135725c8eb56b57728
Carl Parm <carlparm@gmail.com>
2020-01-23 17:29:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,6 +4,8 @@ def map(source_array)
   new_array = []
   i = 0
   while i < source_array.length
-    new_array.push(source_array)
+    new_array.push(yield(source_array[i]))
+    i += 1
   end
+  new_array
 end
