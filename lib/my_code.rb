b354001863adb424b8ae7fcc2d295b47aaab506e
Carl Parm <carlparm@gmail.com>
2020-01-23 17:53:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,13 +12,13 @@ end
 
 def reduce(source_array, starting_value=nil)
   if starting_value
-    num1 = starting_value
+    sum = starting_value
     i = 0
   else
-    num1 = source_array[0]
+    sum = source_array[0]
     i = 1
   end
   
   while i < source_array.length
-    yield(num1, source_array[i])
+    sum = yield(num1, source_array[i])
 end
